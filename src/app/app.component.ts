09d26a15db28791371f3e449ed2bd16596907e20
Omnia Mohamed <74616745+omnia192@users.noreply.github.com>
2024-08-09 10:42:00 +0200

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../services/language.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'marsaV3';
+  title = 'marsa-project';
+  constructor(
+    private langServ: LanguageService,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    const lang = localStorage.getItem('lang');
+    if (lang) {
+      this.langServ.setCurrentLang(lang, true);
+    } else {
+      this.langServ.setCurrentLang('en', true);
+    }
+    this.authService.autoAuth();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ToastrModule } from 'ngx-toastr';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,6 +17,15 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgxMaterialTimepickerModule,
+    ToastrModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [
     provideClientHydration(),
@@ -20,3 +34,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
